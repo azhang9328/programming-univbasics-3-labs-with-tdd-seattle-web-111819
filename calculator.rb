65425a6b9a2ef55efbcffc93001226508b4f8c62
Allen Zhang <azhang9328@gmail.com>
2019-11-15 08:12:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -23,10 +23,8 @@
 #  it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
 #    first_number = get_variable_from_file('./calculator.rb', "first_number")
 #    second_number = get_variable_from_file('./calculator.rb', "second_number")
-#    difference = get_variable_from_file('./calculator.rb', "difference")
-
-    expect(difference).to eq(first_number-second_number)
-  end
+#   expect(difference).to eq(first_number-second_number)
+#  end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
