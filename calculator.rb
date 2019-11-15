373bc933a9eda5e928246e6226fb054c577effb8
Allen Zhang <azhang9328@gmail.com>
2019-11-15 08:13:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -31,16 +31,16 @@
 #    second_number = get_variable_from_file('./calculator.rb', "second_number")
 #    product = get_variable_from_file('./calculator.rb', "product")
 
-    expect(product).to eq(first_number*second_number)
-  end
+#    expect(product).to eq(first_number*second_number)
+#  end
 
-  it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    quotient = get_variable_from_file('./calculator.rb', "quotient")
+#  it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
+#    first_number = get_variable_from_file('./calculator.rb', "first_number")
+#    second_number = get_variable_from_file('./calculator.rb', "second_number")
+#    quotient = get_variable_from_file('./calculator.rb', "quotient")
 
-    expect(quotient).to eq(first_number/second_number)
-  end
-end
+#    expect(quotient).to eq(first_number/second_number)
+#  end
+#end
 
-first_number = 
+first_number = 0 
